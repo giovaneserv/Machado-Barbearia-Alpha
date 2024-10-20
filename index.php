97cb5e4809cf4d3f0085d074cc099e50276e06d1
giovaneserv <giovan2003serv@gmail.com>
2024-10-20 13:10:26 -0300

    Página principal index.php

--- index.php
+++ index.php
@@ -1,28 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Barber Shop</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/animation.css">
-</head>
+    <!DOCTYPE html>
+    <html lang="pt-br">
 
-<body>
-    <?= include 'nav.html';?>
-    <main class="container1">
-        <header>
-            <h1>Bem-vindo ao nosso site</h1>
-           <a href="contato.php"><h2>Marque um horário!</h2></a>
-           <script src="js/form.js"></script>
-        </header>
-    </main>
-    <script>
-        const ul = document.querySelector('ul');
-        const li = document.querySelector('li');
-        
-    </script>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Machado Barbearia</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/animation.css">
+    </head>
 
-</html>
+    <body>
+        <?php include 'nav.php' ?>
+        <main class="container1">
+            <header>
+                <?php
+                /* ↓ se usuario estiver logado, aparecerá o h1 abaixo */
+                if ($usuario_log) {
+                    echo "<h1>Seja bem-vindo, $nome</h1>";
+                } else {
+                    /* ↓ senão */
+                    echo "<h1>Bem-vindo ao nosso site</h1>";
+                    echo "<a href='contato.php'><h2>Marque um horário!</h2></a>";
+                }
+                ?>
+
+
+            </header>
+        </main>
+    </body>
+
+    </html>
